39c97963d7fa98ca068561992867840fdcbc75a1
Florencia Ramirez <a2019110598@estudiantes.upsa.edu.bo>
2020-08-26 18:51:31 -0400

    Creacion web Student

--- APIstudent/APIstudent/Models/DataContext.cs
+++ APIstudent/APIstudent/Models/DataContext.cs
@@ -6,7 +6,7 @@ using System.Web;
 
 namespace APIstudent.Models
 {
-    public class DataContext : DbContext
+    public class DataContext : DbContext//prop tab tab
     {
         public DataContext() : base("DefaultConnection")  //ctor tab tab
         {
